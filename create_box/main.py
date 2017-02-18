c151d87742950ebf34d1d3863dda599f9d1664db
louise <kaelenmedeiros@gmail.com>
2017-02-18 16:50:40 +0000

    added error checks for height and width

--- create_box/main.py
+++ create_box/main.py
@@ -2,7 +2,14 @@
 
 
 def create_box(height, width, character):
-    return (character*width + '\n')*height
+    if height >= 1 and width >= 1:
+        return (character*width + '\n')*height
+    else: 
+        return 'height & width must be greater than or equal to 1'
+
+#Add error checking to make sure the width and height is greater or equal to 1
+#Add an extra unit test for the third case in the tests file
+#make it so only the outer border of the box shows and it is not filled in.
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
